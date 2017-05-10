0125d43c7ba5971a6744f66f5fd02432179a6de1
HwanHee Yang <twityhwan@alticast.com>
2017-05-10 17:35:59 +0900

    Removed unnecessary codes.

--- server.cpp
+++ server.cpp
@@ -5,27 +5,7 @@
 using namespace std;
 
 int main() {
-    char buf[4096];
-    char end[] = "quit";
     ChatServer server;
     server.start();
-
-    /*
-    while(true) {
-        cout<<"<< ";
-        cin>>buf;
-        if(strlen(buf) == 0) {
-            break;
-        }
-
-        if (!strcmp(buf, end)) {
-            cout<<"Server End ..."<<endl;
-            break;
-        }
-        cout<<">> "<<buf<<endl;
-
-    }
-    */
-
     return 0;
 }
